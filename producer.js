d9efcb4a0f44670050c9e1807b257882bd0be62e
jaydipjadhav <jaydipdjadhav>
2024-07-09 20:12:22 +0530

    producer done

---
+++ producer.js
@@ -0,0 +1,28 @@
+const {kafka} = require("./client")
+
+async function init(){
+    const producer = kafka.producer();
+console.log("connecting producer.....")
+ 
+await producer.connect();
+
+console.log("connected producer successfully.....")
+
+await producer.send({
+    topic :'rider-update',
+    messages : [
+        {
+            partition : 0,
+            key : 'loaction-update' , value : JSON.stringify({name : "jaydip" , loc : "pune"})
+        }
+    ]
+})
+
+console.log("producer disconnected.......");
+
+await producer.disconnect();
+
+
+}
+
+init();
